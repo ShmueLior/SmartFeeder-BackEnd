1035b0b5a954695c77f563ea82f1c691e9219eb5
Lior_Shmueli <shmueli@gmail.com>
2020-07-08 20:50:18 +0300

    /api/v1.0/arduino return json now

--- routes/arduino.js
+++ routes/arduino.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /*GET /api/v1.0/arduino */
 router.get('/', function (req, res, next) {
-    res.status(200).send("This will be an array of flag jast for you!");
+    res.status(200).send({ message: "This will be an array of flag jast for you!" });
 });
 
 module.exports = router;
