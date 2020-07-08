dda30a18c1503c5a4be3386a5cce6e14d59b13a2
dekeltssairi <dekeltsairi@gmail.com>
2020-07-08 04:08:11 +0300

    adding authorization request which validating the right token added to the header's request, adding passport template

--- initializers/mongoose.js
+++ initializers/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-//const MONGODB_URI = "mongodb+srv://PetFeeder-Admin:lndd123456@petfeederdb-c5umy.mongodb.net/PetFeederDB?retryWrites=true&w=majority"
+const MONGODB_URI = "mongodb+srv://PetFeeder-Admin:lndd123456@petfeederdb-c5umy.mongodb.net/PetFeederDB?retryWrites=true&w=majority"
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
