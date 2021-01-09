29a4b5ddf7037bb74a6d934a81496445c8f9c032
Lior_Shmueli <shmueli@gmail.com>
2021-01-09 19:47:34 +0200

    Last Deploy

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static('uploads'));
-// app.use("/api", function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// });
+app.use("/api", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(flash());
 app.use(passport.initialize());
 
